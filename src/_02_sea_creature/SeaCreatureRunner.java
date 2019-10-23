6855ddbceb929570e087da663e7def260427c2cc
League Student <league@iMac-37.attlocal.net>
2019-10-23 16:54:21 -0700

    left off on tea party

--- src/_02_sea_creature/SeaCreatureRunner.java
+++ src/_02_sea_creature/SeaCreatureRunner.java
@@ -3,6 +3,15 @@ package _02_sea_creature;
 public class SeaCreatureRunner {
 public static void main(String[] args) {
 	SeaCreature bob = new SeaCreature("SpongeBob");
+	bob.eat();
+	bob.laugh();
+	SeaCreature joe = new SeaCreature("Patrick");
+	joe.getName();
+	joe.eat();
+	joe.laugh();
+	SeaCreature jim = new SeaCreature("Squidward");
+	jim.getName();
+	jim.eat();
+	jim.laugh();
 }
-
 }
---
+++ src/_07_tea_party/TeaParty.java
@@ -0,0 +1,21 @@
+package _07_tea_party;
+
+public class TeaParty {
+	String name;
+	boolean isWoman;
+	boolean isKnited;
+	 String welcome(String name, boolean isWoman, boolean isKnighted) {
+      if(isWoman == true && isKnighted == true) {
+    	  return "Lady";
+      }
+      if(isWoman == true && isKnighted == false){
+    	  return "Ms.";
+      }
+      if(isWoman == false && isKnighted == true) {
+    	  return "Sir";
+      }
+      if(isWoman == false && isKnighted == false){
+    	  return "Mr.";
+      }
+    }
+}
