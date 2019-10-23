8b2c5ccbe7473d9521025a8a0f701846028dd47b
League Student <league@iMac-37.attlocal.net>
2019-10-23 16:54:40 -0700

    Update TeaParty.java

--- src/_07_tea_party/TeaParty.java
+++ src/_07_tea_party/TeaParty.java
@@ -4,7 +4,7 @@ public class TeaParty {
 	String name;
 	boolean isWoman;
 	boolean isKnited;
-	 String welcome(String name, boolean isWoman, boolean isKnighted) {
+	public String welcome(String name, boolean isWoman, boolean isKnighted) {
       if(isWoman == true && isKnighted == true) {
     	  return "Lady";
       }
